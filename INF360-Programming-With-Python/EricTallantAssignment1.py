ef0f0f477cbb29b577f8fa9fc16d0e053c0eef9c
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-08-21 17:47:18 -0500

    Create EricTallantAssignment1.py

---
+++ INF360-Programming-With-Python/EricTallantAssignment1.py
@@ -0,0 +1,12 @@
+# INF360 Programming with Python
+# Eric Tallant
+# Assignment 1
+
+print('Welcome, user. What can I do for you? ')
+response = input()
+
+print('"' + response + '" sounds too difficult.\nHow difficult is it on a scale of 1 - 10? ')
+difficultyScale = int(input())
+
+print('Error: "' + response + '" sounds more like a ' + str(difficultyScale * 10))
+print('Choose something closer to ' + str(difficultyScale - 2) + ' next time.\nGoodbye.')
