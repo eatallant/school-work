2d5f5a3a84a5ffacaeb349dc6d2380aab55ca360
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-08-21 19:22:12 -0500

    Update EricTallantAssignment1.py
    fixed grammatical error

--- INF360-Programming-With-Python/EricTallantAssignment1.py
+++ INF360-Programming-With-Python/EricTallantAssignment1.py
@@ -8,5 +8,5 @@ response = input()
 print('"' + response + '" sounds too difficult.\nHow difficult is it on a scale of 1 - 10? ')
 difficultyScale = int(input())
 
-print('Error: "' + response + '" sounds more like a ' + str(difficultyScale * 10))
+print('Error: "' + response + '" sounds more like ' + str(difficultyScale * 10))
 print('Choose something closer to ' + str(difficultyScale - 2) + ' next time.\nGoodbye.')
