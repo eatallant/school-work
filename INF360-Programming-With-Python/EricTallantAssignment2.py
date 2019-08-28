88bfcd5ada0ead3108861806c99fa99a5b827783
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-08-28 14:36:57 -0500

    Create EricTallantAssignment2.py

---
+++ INF360-Programming-With-Python/EricTallantAssignment2.py
@@ -0,0 +1,13 @@
+# INF360 - Programming in Python
+# Eric Tallant
+# Assignment 2
+
+#(1 point) - Initial comments*
+#(1 points) - Use at least 2 comparison operators.
+#(1 points) - Use at least 1 boolean operator (and, or, not).
+#(2 points) - Write at least 1 if statement that contains 2 elif statements and 1 else statement.
+#(2 points) - Write at least 1 while statement that contains a break and continue.
+#(2 points) - Write at least 1 for loop using the range() method.
+#(1 point) - Use the import keyword to import the random module. Use the random.randint() function somewhere in your script.
+
+print('Welcome to the number machine')
