98996ea90c0a9db5b01a3da764e46743e4aea247
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-08-30 15:13:59 -0500

    Update EricTallantAssignment2.py

--- INF360-Programming-With-Python/EricTallantAssignment2.py
+++ INF360-Programming-With-Python/EricTallantAssignment2.py
@@ -10,4 +10,27 @@
 #(2 points) - Write at least 1 for loop using the range() method.
 #(1 point) - Use the import keyword to import the random module. Use the random.randint() function somewhere in your script.
 
-print('Welcome to the number machine')
+
+
+print("Hello, enter a number between 1 and 10 and I will guess it (hint, I'll know if you pick 5): ")
+number = int(input())
+
+while number < 1 or number > 10:
+  print("Follow the rules and try again: ")
+  number = int(input())
+
+
+
+while False: # while false, keep guessing
+
+  low = 1
+  high = 11
+
+  guessRange = range(low,high)
+
+  if number == 5: # if number is 5, guess 5
+    print('You for sure picked 5')
+  elif number < 5: # else if number is less than 5, guess the highest remaining number and dec high
+    print('I\'m thinking ' + str(low))
+# else if number is greater than 5, guess the lowest remaining number and inc low
+# else give error and quit
