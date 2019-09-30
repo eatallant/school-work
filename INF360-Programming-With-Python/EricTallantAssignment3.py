88d7b6c5f2ca34b14765b7b86165ec67942dc62d
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-09-29 19:21:52 -0500

    Update EricTallantAssignment3.py

--- INF360-Programming-With-Python/EricTallantAssignment3.py
+++ INF360-Programming-With-Python/EricTallantAssignment3.py
@@ -15,4 +15,25 @@
 # cherries Carol moose
 #   banana David goose
 
-def printTable():
+
+
+def printTable(theList):
+
+  # create a list to hold the char amounts for rjust
+  colWidths = [0] * len(theList)
+
+  # populate colWidths with the longest string in each list
+  for i in range(len(theList)):
+    for j in theList[i]:
+      if(len(j) > colWidths[i]):
+        colWidths[i] = len(j)
+
+
+  # print the nth item in each list right justified in each line
+  for i in range(len(theList[0])):
+    for j in range(len(theList)):
+      print(theList[j][i].rjust(colWidths[j]), end=' ')
+    print('')
+
+
+
