e5099712a2783ccfda77aa503d7a0e73e09d3fae
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-09-29 19:22:32 -0500

    Update EricTallantAssignment3.py

--- INF360-Programming-With-Python/EricTallantAssignment3.py
+++ INF360-Programming-With-Python/EricTallantAssignment3.py
@@ -1,3 +1,9 @@
+
+# INF360 - Programming in Python
+# Eric Tallant
+# Assignment 3
+
+
 # Write a function named printTable() that takes a list of lists of strings
 # and displays it in a well-organized table with each column right-justified. 
 # Assume that all the inner lists will contain the same number of strings. 
