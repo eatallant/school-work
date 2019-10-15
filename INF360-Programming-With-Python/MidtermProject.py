879c5c637ad7803048c1cb51517f5bec4eac0fa0
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-10-15 14:58:21 -0500

    Update MidtermProject.py

--- INF360-Programming-With-Python/MidtermProject.py
+++ INF360-Programming-With-Python/MidtermProject.py
@@ -1,3 +1,9 @@
 # INF360 - Programming with Python
 # Eric Tallant
 # Midterm Project
+
+# PROJECT IDEA BLOCK ****ERASE THIS BEFORE TURNING IN*****
+# imports: requests
+# 
+# User enters a website that they want info from. Use requests to verify that this is a real address
+# if it is, get some kind of info, else give an error
