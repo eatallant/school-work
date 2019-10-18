068b193dbe4f0f4d4157c093a109c53151474ca8
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-10-17 20:11:15 -0500

    Update MidtermProject.py

--- INF360-Programming-With-Python/MidtermProject.py
+++ INF360-Programming-With-Python/MidtermProject.py
@@ -7,3 +7,19 @@
 # 
 # User enters a website that they want info from. Use requests to verify that this is a real address
 # if it is, get some kind of info, else give an error
+
+
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+url = 'https://www.theonion.com/'
+
+r1 = requests.get(url)
+coverpage = r1.content
+
+soup1 = BeautifulSoup(coverpage, 'lxml')
+
+coverpage_news = soup1.find_all('h1', class_='leHuzq')
+
+print(coverpage_news)
