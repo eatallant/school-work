68b56648fa0d384999bda1012db000e6bfe5ef1a
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-10-18 21:06:39 -0500

    Update MidtermProject.py

--- INF360-Programming-With-Python/MidtermProject.py
+++ INF360-Programming-With-Python/MidtermProject.py
@@ -5,28 +5,44 @@
 # PROJECT IDEA BLOCK ****ERASE THIS BEFORE TURNING IN*****
 # imports: requests
 # 
-# User enters a website that they want info from. Use requests to verify that this is a real address
-# if it is, get some kind of info, else give an error
+# Pulls new article headlines from the front page of the onion.
+# User then selects an article number and the body of that article is queried and displayed
 
 
 import requests
 import lxml
+import re
 from bs4 import BeautifulSoup
 
 url = 'https://www.theonion.com/'
 
+
 r1 = requests.get(url)
-coverpage = r1.content
+homepage = r1.content
 
-soup1 = BeautifulSoup(coverpage, 'lxml')
+soup1 = BeautifulSoup(homepage, 'lxml')
 
-coverpage_news = soup1.find_all('h1', class_='leHuzq')
+# this will find every article title in the new article div on the page
+articles = soup1.find_all('h1', class_='leHuzq')
+articleLink = soup1.find_all('a', class_='sc-1out364-0')
 
-# loop through each article that coverpage_news finds and print the result
-for article in range(len(coverpage_news)):
-  
-  title = str(coverpage_news[article]).split('>')
-  print(title[1])
+
+for title in range(len(articles)):
+  print(articleLink[title])
+  print(articles[title])
+
+# loop through each article that homepage finds and print the result
+for title in range(len(articles)):
+
+  # use a regex to remove the html tags from the string
+  titleText = re.split('<|>', str(articles[title]))
+  print(str(title + 1) + ': "' + titleText[2] + '"\n')
+
+
+
+
+
+
 
 
 
