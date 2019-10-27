77e3aa7e85a26a838707897fb8a8f91c3b06e9a0
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-10-27 17:06:00 -0500

    Update EricTallantAssignment4.py

--- INF360-Programming-With-Python/EricTallantAssignment4.py
+++ INF360-Programming-With-Python/EricTallantAssignment4.py
@@ -25,5 +25,21 @@ The silly panda walked to the chandelier and then screamed. A nearby pickup
 truck was unaffected by these events.
 
 The results should be printed to the screen and saved to a new text file.
+
+functions:
+1. getLibs(textFile)
+  take file and run it through regex. every time ADJECTIVE, ADVERB, NOUN, or VERB is found in the file, 
+    ask user to input that word. append that input to list
+  preconditions: textFile exists
+  postconditions: return list of user input values
+  
+program steps:
+1. get file from user
+2. run getLibs
+3. create new file
+4. copy the contents of existing file to new file
+5. replace libs with user input
+6. print file
+7. close file
 """
 
