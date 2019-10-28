e283e6d9cd40ea4ea3ffe589a9b4364c462d19d1
Eric Tallant <41919614+eatallant@users.noreply.github.com>
2019-10-27 20:12:43 -0500

    Update EricTallantAssignment4.py

--- INF360-Programming-With-Python/EricTallantAssignment4.py
+++ INF360-Programming-With-Python/EricTallantAssignment4.py
@@ -25,68 +25,64 @@ The silly panda walked to the chandelier and then screamed. A nearby pickup
 truck was unaffected by these events.
 
 The results should be printed to the screen and saved to a new text file.
-
-functions:
-1. makeLib(textFile, newLibName)
-  create file newLibName.txt. copy contents of textFile. take new file and run it through regex. 
-    every time ADJECTIVE, ADVERB, NOUN, or VERB is found in the file, 
-    ask user to input that word type. replace that word type with user input. close file at end
-  preconditions: textFile exists
-  
-program steps:
-1. get file from user
-2. run getLibs
-3. print file
 """
+
 import os
 import re
+import sys
 
 
 # create a new file that is an exact copy of textFile
 def make_lib_file(textFile, newLibName):
-  # validate file
-  if not os.path.isfile(textFile):
-    print('File or directory does not exist')
-    return
+    # validate file
+    if not os.path.isfile(textFile):
+        sys.exit('File or directory does not exist')
 
-  # open file
-  textContent = textFile.open()
+    # open file
+    textContent = open(textFile)
 
-  # create new file and copy contents of textFile
-  newLib = open(newLibName + '.txt', 'w')
-  newLib.write(textContent.read())
-  newLib.close
+    # create new file and copy contents of textFile
+    newLib = open(newLibName, 'w')
+    newLib.write(textContent.read())
+    newLib.close
 
 
 # make a list of libs that need answers from file
 def madLibber(textFile):
 
-  libContent = textFile.open()
-  # make a list of words that need filled in
-  libsWanted = re.findall('ADJECTIVE'|'ADVERB'|'NOUN'|'VERB', libContent)
+    libContent = open(textFile)
+    content = libContent.read()
+    libContent.close()
+    # make a list of words that need filled in
+    libsWanted = re.findall('ADJECTIVE|ADVERB|NOUN|VERB', content)
 
-  # make a list of the users choice for the words
-  userLibs = []
-  for word in libsWanted:
-    userLibs.append(input('enter ' + word.lower() + ': '))
+    # make a list of the users choice for the words
+    userLibs = []
+    for word in libsWanted:
+        userLibs.append(input('enter ' + word.lower() + ': '))
 
-  # for each word user picked, replace the place holder in the text file with the word
-  for word in userLibs:
-    libContent.write(re.sub('ADJECTIVE'|'ADVERB'|'NOUN'|'VERB', word, libContent))
+    # for each word user picked, replace the place holder in the text file with the word
+    for word in userLibs:
+        content = re.sub('ADJECTIVE|ADVERB|NOUN|VERB', word, content, 1)
 
-  libContent.close()
+    # overwrite textFile with content
+    libContent = open(textFile, 'w')
+    libContent.write(content)
+    libContent.close()
 
+
+# This is the main program. Get path from user and have them name a new file
 print('Welcome to Mad Lib')
-libFile = input('Enter the path of your file: ')
-newLib = input('Make a new name for this mad lib: ')
+
+libFile = str(input('Enter the path of your file: '))
+newLib = str(input('Make a new name for this mad lib: ') + '.txt')
 
 make_lib_file(libFile, newLib)
 madLibber(newLib)
 
-newLib.open()
-print(newLib.read())
-
-
-
+# print newly edited text file
+newLibText = open(newLib)
+print(newLibText.read())
+newLibText.close()
 
 
